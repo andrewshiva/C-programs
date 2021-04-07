57a2e0b244c8fea2ed9f8bf95dcc488475b188e0
andrewshiva <andrewshiva886@gmail.com>
2021-04-07 12:16:02 +0530

    new

---
+++ Assignmentc/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 0;
+        int j = 0;
+
+        int[] arr1 = new int[5] {
+         3,
+         27,
+         4,
+         99,
+         54
+      };
+        int[] arr2 = new int[5] {
+         105,
+         55,
+         1,
+         77,
+         95
+      };
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+        int[] merged = new int[10];
+        int k = 0;
+        for (i = 0; i < 10; i++)
+        {
+            if (arr1[j] > arr2[k])
+            {
+                merged[i] = arr2[k];
+                k = k + 1;
+            }
+            else
+            {
+                merged[i] = arr1[j];
+                if (j != 4)
+                {
+                    j = j + 1;
+                }
+            }
+        }
+        Console.WriteLine("Elements of merged array = ");
+        for (i = 0; i < 10; i++)
+        {
+            Console.WriteLine("merged[" + (i) + "]: " + merged[i]);
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ enum program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Example
+{
+    public enum Days
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
+
+    public static void Main()
+    {
+        List<Days> days = Enum.GetValues(typeof(Days))
+                            .Cast<Days>()
+                            .ToList();
+
+        Console.WriteLine(String.Join(Environment.NewLine, days));
+    }
+}
